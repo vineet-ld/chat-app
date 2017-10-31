5c9a4c3317ca4537ba6e1a6d82d53fb2151d5208
Vineet Dandekar <vineet_ld@live.com>
2017-10-31 14:36:07 -0400

    Sets up Socket.io client and server web sockets

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const PUBLIC_PATH = path.join(__dirname, "../public");
 const PORT = process.env.PORT || 3002;
@@ -9,7 +11,22 @@ var app = express();
 // Setup express to access the public folder
 app.use(express.static(PUBLIC_PATH));
 
+// Setup http server manually
+var server = http.createServer(app);
+
+// Setup Socket.io
+var io = socketIO(server);
+
+// Soclet.io event handlers
+io.on("connection", (socket) => {
+    console.log("Connected to the client");
+
+    socket.on("disconnect", () => {
+        console.log("Disconnected from the client");
+    });
+});
+
 // Starting express server
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
 });
